f353e30c783e19b5a23972c4b7beed6b07e6143b
tiggerbob <tiggerbob@gmail.com>
2023-03-07 22:27:58 -0600

    base app

---
+++ index.js
@@ -0,0 +1,22 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+  io.emit('server message', 'a user has connected!');
+  socket.on('disconnect', () => {
+    io.emit('server message', "a user has disconnected :(");
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
