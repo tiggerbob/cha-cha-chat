b637372d06900ee2d131a291dc88edeb3f882118
tiggerbob <tiggerbob@gmail.com>
2023-03-13 20:39:45 -0500

    styling and status messages integration

--- index.js
+++ index.js
@@ -3,17 +3,27 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
+var usernames = [];
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+  socket.on('send username', username => {
+    socket.username = username;
+    usernames.push(socket.username);
+    socket.emit('server message', 'currently online: ' + usernames.join(', '));
+    socket.broadcast.emit('server message', socket.username + ' joined');
   });
-  io.emit('server message', 'a user has connected!');
+
+  socket.on('chat message', msg => {
+    io.emit('chat message', socket.username + ": " + msg);
+  });
+
   socket.on('disconnect', () => {
-    io.emit('server message', "a user has disconnected :(");
+    io.emit('server message', socket.username + " left");
+    usernames = usernames.filter(e => e !== socket.username);
   });
 });
 
